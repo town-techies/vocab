535270bb9c8a3b381f612c1f8b77dabe66e8d04e
tt-09 <tt-09@tt-09.(none)>
2013-02-01 12:53:29 +0530

    latest cahnges

--- app/controllers/vocab_api_controller.rb
+++ app/controllers/vocab_api_controller.rb
@@ -222,6 +222,8 @@ class VocabApiController < ApplicationController
     @uid = params['uid'];
     @id = params["id"];
     @user_puzzles_paid = []
+    @user_puzzles_advance = []
+    @user_puzzles = []
     if(!params["pid"].nil? && !params["uid"].nil?) 
 	if(!params["id"].nil?)
 		@user_puzzles = UserPuzzle.find(:all,:conditions => ['device_id =? and puzzle_id = ? and id= ? ',@uid,@pid,@id])
@@ -232,11 +234,27 @@ class VocabApiController < ApplicationController
     else 
       @user_puzzles = UserPuzzle.find(:all,:conditions => ['device_id =? ',@uid])
     end
+    #render :text =>  @user_puzzles.inspect and return false
     @user_puzzles.each do |user_puzzles|
      
       @puzzle_detail = user_puzzles.puzzle
       
-      info = {}
+      if @puzzle_detail.category == 'advance'
+	  info = {}
+      info["id"] = user_puzzles.id
+      info["device_id"] = user_puzzles.device_id
+      info["puzzle_id"] = user_puzzles.puzzle_id
+      info["score"] = user_puzzles.score
+      info["correct_answer"] = user_puzzles.correct_answer
+      info["wronge_answer"] = user_puzzles.wronge_answer
+      info["created_at"] = user_puzzles.created_at
+      info["updated_at"] = user_puzzles.updated_at
+      info["question_detail"] = user_puzzles.question_detail
+      info["paid"] = @puzzle_detail.paid if !@puzzle_detail.nil?
+
+      @user_puzzles_advance << info
+      else
+	  info = {}
       info["id"] = user_puzzles.id
       info["device_id"] = user_puzzles.device_id
       info["puzzle_id"] = user_puzzles.puzzle_id
@@ -249,12 +267,29 @@ class VocabApiController < ApplicationController
       info["paid"] = @puzzle_detail.paid if !@puzzle_detail.nil?
 
       @user_puzzles_paid << info
+	end
+	
+
     end
-  
+    #render :text =>  @user_puzzles_paid.inspect and return false
+
+  if params['category'] == 'advance'
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json  { render :json => @user_puzzles_advance}
+    end
+
+  else
     respond_to do |format|
       format.html # index.html.erb
       format.json  { render :json => @user_puzzles_paid}
     end
+  end
+  #render :text =>  @user_puzzles.inspect and return false 
+   # respond_to do |format|
+   #   format.html # index.html.erb
+   #   format.json  { render :json => @user_puzzles}
+   # end
   end  
 
   def getAllQuestionwithAnswer
